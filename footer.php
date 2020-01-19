e9c95338f0468f79529c4fde137c40ca94de6988
Muazzam Younus <muazzam.younus925@gmail.com>
2020-01-20 04:05:50 +0500

    First Commit

---
+++ footer.php
@@ -0,0 +1,37 @@
+<!-- footer -->
+<footer>
+    <div class="footer-wrapper w-100 bg-blue-exdark d-flex align-content-center justify-content-center flex-column flex-md-row">
+        <span class="copyright color-blue d-flex align-content-center justify-content-center px-1">
+            <span class="align-self-center">&copy; <?php echo date("Y"); ?> Exact Energy. All Rights Reserved.</span>
+        </span>
+        <span class="footer-logo px-2 d-flex align-content-center justify-content-center">
+            <img class="img-fluid img-logo align-self-center" src="assets/images/logo.png" alt="Footer Logo">
+        </span>
+        <span class="footer-url color-blue d-flex align-content-center justify-content-center px-1">
+            <span class="align-self-center">www.exact-energy.com</span>
+        </span>
+    </div>
+</footer>
+<!-- /footer -->
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+
+<!-- jQuery & jQuery UI script -->
+<script src="assets/js/jquery-3.3.1.min.js"></script>
+<script src="assets/js/jquery-ui.min.js"></script>
+
+<!-- Popper script -->
+<script src="assets/js/popper.min.js"></script>
+
+<!-- Bootstrap core scripts -->
+<script src="assets/js/bootstrap.min.js"></script>
+
+<!-- Place your scripts here-->
+<script src="assets/js/custom/script.js"></script>
+<script src="assets/js/custom/jq-script.js"></script>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <meta name="author" content="Exact Energy">
+
+    <meta name="description" content="Exact Energy Solutions Ltd">
+
+    <link rel="icon" href="assets/images/favicon.ico" type="image/x-icon">
+
+    <title>Home - Exact Energy</title>
+
+
+    <!-- jQuery UI CSS -->
+    <link rel="stylesheet" href="assets/css/jquery-ui.min.css"/>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="assets/css/all.min.css">
+
+    <!-- Normalize CSS -->
+    <link href="assets/css/normalize.css" rel="stylesheet" type="text/css">
+
+    <!-- Place your stylesheet here-->
+    <link rel="stylesheet" type="text/css" href="assets/css/custom/style.css">
+
+
+</head>
+
+<body>
